49bb7eacf3f9ee1c2048ee8ef0746d5cc568ac79
Bryan Hilmer <bhilmer@gmail.com>
2012-12-19 20:24:45 -0800

    Adds function stubs
    Mostly just psuedo-code at this point.  Real trick will be implimenting
    this...

--- backlash.js
+++ backlash.js
@@ -2,11 +2,39 @@
  *
  */
 define('backlash', ['underscore', 'backbone', 'jquery'], function(_, Backbone, $){
-	var BindingView = Backbone.View.extend({
+	var BindingMapper = {
+		parseBindings: function(){
+
+		}
+	};
+
+	return Backbone.View.extend({
 		constructor: function(){
 			Backbone.View.prototype.constructor.apply(this, arguments);
+			this._srcRender = this.render;
+			this.render = this._render;
+		},
+
+		_render: function(){
+			this._srcRender();
+			this.setBindings();
+			return this;
+		},
+
+		setBindings: function(){
+			var bindings = BindingMapper.parseBindings();
+
+			//Removes existing bindings
+			//TODO: make this only remove backlash events
+			this.stopListening(this.model);
+
+			for(var binding in bindings){
+				this.listenTo(model, 'change:'+binding.attr, this.renderFragment);
+			}
+		},
+
+		renderFragment: function(){
+
 		}
 	});
-
-	return BindingView;
 });
